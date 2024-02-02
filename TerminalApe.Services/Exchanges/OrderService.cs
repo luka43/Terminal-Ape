0f8e885c04547f577379d76a5d7a07bc740f28d7
luka43 <102422999+luka43@users.noreply.github.com>
2024-02-02 11:11:31 +0100

    Added "Ape" timers and Order Create and Sign for binance

---
+++ TerminalApe.Services/Exchanges/OrderService.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using TerminalApe.Models.Exchange;
+using TerminalApe.Models.Configuration;
+
+namespace TerminalApe.Services.Exchanges;
+
+public class OrderService
+{
+    private ExchangeSettings exchangeSettings { get; } = new ExchangeSettings();
+    
+
+    public async Task<dynamic> Execute(string exchange, string secretKey, Order order)
+    {
+
+        // Sign order
+
+        if (exchange == "Binance")
+        {
+            string timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString();
+            string queryString =
+                $"symbol={order.Base + order.Quote}" +
+                $"&side={order.Side}" +
+                $"&type={order.Type}" +
+                $"&quoteOrderQty={order.Size}" +
+                $"&timestamp={timestamp}";
+
+            string header = "X-MBX-APIKEY";
+
+            string signature = Sign(secretKey, queryString);
+
+            using (HttpClient client = new HttpClient())
+            {
+                // Set the base URL for the Binance API
+                try
+                {
+                    client.BaseAddress = new Uri(exchangeSettings.Default()[exchange].APIUrlBase);
+
+                    // Add the API key and signature to the request headers
+                    client.DefaultRequestHeaders.Add(header, exchangeSettings.Default()[exchange].APIKey);
+                    queryString += $"&signature={signature}";
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"({order}) Error: {ex.Message}");
+                }
+                // Send the POST request to create a market order
+                HttpResponseMessage response = await client.PostAsync($"{exchangeSettings.Default()[exchange].APIUrlOrder}?{queryString}", null);
+
+                // Read the response content
+                string jsonContent = await response.Content.ReadAsStringAsync();
+                dynamic data = JObject.Parse(jsonContent);
+                try
+                {
+                    if (data.status.ToString() == "FILLED")
+                    {
+                        Console.WriteLine($"Order {data["status"]}");
+                        Console.WriteLine($"Symbol: {data.symbol}");
+                        Console.WriteLine($"Side: {data.side}");
+                        Console.WriteLine($"Type: {data.type}");
+                        Console.WriteLine($"Base Quantity: {data.executedQty}");
+                        Console.WriteLine($"Price: {data.fills[0].price}");
+
+                        return data;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"({order}) Error code for : {data.code}");
+                        Console.WriteLine($"({order}) Error code: {data.msg}");
+                        return data;
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine($"({order}) Error code for : {data.code}");
+                    Console.WriteLine($"({order}) Error code: {data.msg}");
+
+                    return data;
+                }
+            }
+        }
+        else
+        {
+            return null;
+        }        
+    }
+
+    private string Sign(string apiSecret, string data)
+    {
+        using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(apiSecret)))
+        {
+            byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+
+    }
+}
--- TerminalApe.UI/Services/Timers.cs
+++ TerminalApe.UI/Services/Timers.cs
@@ -1,24 +1,49 @@
-﻿using TerminalApe.Models.Configuration;
+﻿using TerminalApe.DAL;
+using TerminalApe.Models.Configuration;
+using TerminalApe.Models.Exchange;
 using TerminalApe.Services.Connection;
+using TerminalApe.Services.Exchanges;
 using TerminalApe.UI.Services;
 
 namespace TerminalApe.UI.Services;
 
 public class Timers
 {
+    private FileDatabase fileDatabase;
+    private PairService pairService;
     private UpdateUi updateUi;
-    private AppSettings appSettings;
-    public System.Windows.Forms.Timer connectionCheckTimer {  get; set; }
+
+    private AppSettings appSettings { get; set; } = new AppSettings().Default();
+    private Dictionary<string, ExchangeSettings> exchangeSettings { get; set; } = new ExchangeSettings().Default();
+    private MarketCache oldMarketCache { get; set; } = new MarketCache();
+    private MarketCache newMarketCache { get; set; } = new MarketCache();
+    private MarketCache newPairsCache { get; set; } = new MarketCache();
+    private dynamic rawMarketCache { get; set; }
+
+
+    public System.Windows.Forms.Timer connectionCheckTimer { get; set; } = new System.Windows.Forms.Timer();
+    public Dictionary<string, System.Windows.Forms.Timer> exchangeTimers { get; set; } = new Dictionary<string, System.Windows.Forms.Timer>();
+
 
     public Timers(MainAppForm form)
     {
+        fileDatabase = new FileDatabase();
+        pairService = new PairService();
         updateUi = new UpdateUi(form);
-        appSettings = new AppSettings().Default();
 
-        connectionCheckTimer = new System.Windows.Forms.Timer();
         connectionCheckTimer.Interval = 1500;
         connectionCheckTimer.Tick += ConnectionCheckTimer_Tick;
         connectionCheckTimer.Start();
+
+        exchangeTimers.Clear();
+
+        foreach (string exchange in appSettings.Exchanges)
+        {
+            exchangeTimers[exchange] = new System.Windows.Forms.Timer();
+            exchangeTimers[exchange].Interval = exchangeSettings[exchange].TIMEOUT;
+            exchangeTimers[exchange].Tick += ApeTimer_Tick;
+            exchangeTimers[exchange].Start();
+        }
     }
 
     private void ConnectionCheckTimer_Tick(object sender, EventArgs e)
@@ -29,5 +54,21 @@ public class Timers
         {
             updateUi.ExchangeLatencyStatus(exchange);
         }        
+    }
+    private void ApeTimer_Tick(object sender, EventArgs e)
+    {
+        foreach(string exchange in appSettings.Exchanges)
+        {
+            ApeTimerService(exchange);
+        }
+    }
+    private async void ApeTimerService(string exchange)
+    {
+        oldMarketCache = fileDatabase.LoadExchangeCache(exchange, Path.Combine(appSettings.DatabaseDirectory, appSettings.DatabaseFile));
+        rawMarketCache = await pairService.GetPairs(exchange);
+        newMarketCache = pairService.SortPairs(exchange, rawMarketCache.result);
+        newPairsCache = pairService.Compare(exchange, oldMarketCache, newMarketCache);
+
+        updateUi.ExchangePairsAndSymbols(exchange, oldMarketCache, newMarketCache, newPairsCache);
     }
 }
